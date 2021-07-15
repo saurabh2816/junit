2445bc9aca0c8d59022ccb5a2fa2bd28b0e892bc
saurabh2816 <saurabh2816#>
2021-07-15 11:16:54 -0700

    first commit

---
+++ src/main/java/com/saurabh/MathUtils.java
@@ -0,0 +1,8 @@
+package com.saurabh;
+
+public class MathUtils {
+
+	public int add(int a, int b) {
+		return a+b;
+	}
+}
---
+++ src/test/java/com/saurabh/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package com.saurabh;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		assertEquals("saurabh", "saurabh");;
+	}
+
+}
