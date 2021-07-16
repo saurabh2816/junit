7358cfbf3aa63405eae640b4aee1323e148003fc
saurabh2816 <saurabh2816#>
2021-07-15 20:47:28 -0700

    @DisplayNameh

--- src/test/java/com/saurabh/MathUtilsTest.java
+++ src/test/java/com/saurabh/MathUtilsTest.java
@@ -5,8 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class MathUtilsTest {
 
 	MathUtils mu;
@@ -16,29 +19,27 @@ class MathUtilsTest {
 		mu = new MathUtils();
 	}
 	
+	
 	@BeforeAll 
 	static void testBeforeAllMethod() {
 		System.out.println("Before all");
 	}
 
 	@Test
+	@DisplayName("Add")
 	void testAdd() {
-	
 		int actual = mu.add(10, 20);
 		assertEquals(30, actual);
 	}
 	
 	@Test
 	void testComputeCircleArea() {
-		MathUtils mu = new MathUtils();
 		double actual = mu.computeCircleArea(10);
 		assertEquals(314, actual);
 	}
 	
 	@Test
 	void testDivide() {
-		MathUtils mu = new MathUtils();
-		
 		assertThrows(ArithmeticException.class , () -> {
 			mu.divide(10,0);	
 		}, "asset throws !!");
