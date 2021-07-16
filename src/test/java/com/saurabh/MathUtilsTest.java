90d89897af6cf4ed5935888646c2099ea44214b5
saurabh2816 <saurabh2816#>
2021-07-15 19:49:32 -0700

    @beforeall & @beforeeach

--- src/test/java/com/saurabh/MathUtilsTest.java
+++ src/test/java/com/saurabh/MathUtilsTest.java
@@ -1,14 +1,29 @@
 package com.saurabh;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
+	MathUtils mu;
+	
+	@BeforeEach
+	void init() {
+		mu = new MathUtils();
+	}
+	
+	@BeforeAll 
+	static void testBeforeAllMethod() {
+		System.out.println("Before all");
+	}
+
 	@Test
 	void testAdd() {
-		MathUtils mu = new MathUtils();
+	
 		int actual = mu.add(10, 20);
 		assertEquals(30, actual);
 	}
