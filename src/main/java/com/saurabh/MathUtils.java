bd7497bc4f2e171a6a339dafc9f3aa791a450ced
saurabh2816 <saurabh2816#>
2021-07-17 21:47:47 -0700

    removing non testing code of streams

--- src/main/java/com/saurabh/MathUtils.java
+++ src/main/java/com/saurabh/MathUtils.java
@@ -31,49 +31,7 @@ public class MathUtils {
 		return 3.14*radius*radius;
 	}
 	
-	public static void main(String args[]) {
-		Map<Integer, Long> mp = Stream.of(1,1,2,3,4,4)
-				.collect(
-						Collectors.groupingBy(
-						Function.identity(),
-						Collectors.counting()))
-				;
-		
-//		mp.entrySet()
-		
-		Stream.of(mp.entrySet().toArray(new Map.Entry[0]))
-			.filter(map -> ((Long)map.getValue() == 1))
-			.forEach(m -> System.out.println(m));
-		int[] a = {10,2,3,4};
-
-		
-		// For primitive
-		IntStream s = Arrays.stream(a);
-		s.filter(w -> w % 2 == 0)
-		.forEach(ar -> System.out.println(ar));
-
-		
-		List<Integer> streamInt = IntStream.of(1,2,3,4)
-		.boxed()
-		.collect(Collectors.toList());
-		
-
-		
-//		
-		Stream<Map.Entry<Integer, Long>> stream = Stream.of(mp.entrySet().toArray(new Map.Entry[mp.size()]));
-//		stream.forEach( m -> System.out.println(m.getKey()+ "-> " + m.getValue()));
-		stream.filter(map -> map.getValue()==1 )
-		.forEach(m -> System.out.println(m));
-		
-		mp.entrySet().stream()
-			.filter( m -> m.getValue()==1 )
-			.forEach(m -> System.out.println(m));
-		
-		mp.forEach((k,v) -> {
-			if(v==1)
-				System.out.println(k);
-		});
-	
+	public static void main(String args[]) {	
 		
 	}
 	
